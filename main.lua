749e65a34bb48baa6fe8ffecdc1f8ab2baad622c
Christian Nelson <zero@zerolives.org>
2009-03-01 21:34:16 -0500

    Sprites for everything, shifter added.

--- main.lua
+++ main.lua
@@ -1,4 +1,8 @@
 function load()
+	love.filesystem.include "Console.lua"
+	love.filesystem.include "Client.lua"
+	love.filesystem.include "Server.lua"
+
 	role = "none"
 	message = "nobody's here"
 	remoteMessage = ""
@@ -6,20 +10,25 @@ function load()
 	status = "not connected"
 	hasExited = 'yes'
 	menuItems = {}
-	love.filesystem.include "Console.lua"
-	love.filesystem.include "Client.lua"
-	love.filesystem.include "Server.lua"
+	shifter_position = 3
+	
 	font = love.graphics.newFont(love.default_font, 14)
 	coolfont = love.graphics.newFont('fonts/orange-kid.ttf', 18)
 	menufont = love.graphics.newFont('fonts/orange-kid.ttf', 25)
 	love.graphics.setFont(coolfont)
-	big_logo = love.graphics.newImage("images/logo.png")
-	metal_bg = love.graphics.newImage("images/grating-bg.png")
-	main_menu_bg = love.graphics.newImage("images/main_menu.png")
+	
+	battle_view_bg = love.graphics.newImage("images/battle_view_bg.png",2)
+	big_logo = love.graphics.newImage("images/logo.png",2)
+	metal_bg = love.graphics.newImage("images/grating-bg.png",2)
+	main_menu_bg = love.graphics.newImage("images/main_menu.png",2)
+	
 	clicktostart = love.graphics.newAnimation( "images/clicktostart.png", 250, 50, .5, 2)
+	
+	shifter = love.graphics.newAnimation( "images/shifter_frames.png", 200, 236, 0, 0)
+	
 	background = metal_bg
 	
-	love.audio.setVolume(.2)
+	--love.audio.setVolume(.2)
 	--menu_effect_1 = love.audio.newSound('sound/menu_effect_1.ogg')
 	--menu_effect_2 = love.audio.newSound('sound/menu_effect_2.ogg')
 	--menu_bg_music = love.audio.newSound('sound/COCK ATTACK.ogg')
@@ -27,13 +36,21 @@ function load()
 	mainMenu()
 	
 	-- Default HUD colors
-	player_head = love.graphics.newImage("images/player_head_green.png")
-	player_body = love.graphics.newImage("images/player_body_green.png")
-	player_l_arm = love.graphics.newImage("images/player_arm_green.png")
-	player_r_arm = love.graphics.newImage("images/player_arm_green.png")
-	player_l_leg = love.graphics.newImage("images/player_leg_green.png")
-	player_r_leg = love.graphics.newImage("images/player_leg_green.png")
-	playerData = {head = 1, l_arm=1, r_arm=1, body=1, l_leg=1, r_leg=1}
+	player_head = love.graphics.newAnimation( "images/head_frames.png", 30, 30, 0, 0 )
+	player_body = love.graphics.newAnimation( "images/body_frames.png", 100, 100, 0, 0 )
+	player_l_arm = love.graphics.newAnimation( "images/arm_frames.png", 25, 50, 0, 0 )
+	player_r_arm = love.graphics.newAnimation( "images/arm_frames.png", 25, 50, 0, 0 )
+	player_l_leg = love.graphics.newAnimation( "images/leg_frames.png", 10, 25, 0, 0 )
+	player_r_leg = love.graphics.newAnimation( "images/leg_frames.png", 10, 25, 0, 0 )
+	playerData = {head = 0, l_arm=0, r_arm=0, body=0, l_leg=0, r_leg=0}
+	
+	cpu_head = love.graphics.newAnimation( "images/head_frames.png", 30, 30, 0, 0 )
+	cpu_body = love.graphics.newAnimation( "images/body_frames.png", 100, 100, 0, 0 )
+	cpu_l_arm = love.graphics.newAnimation( "images/arm_frames.png", 25, 50, 0, 0 )
+	cpu_r_arm = love.graphics.newAnimation( "images/arm_frames.png", 25, 50, 0, 0 )
+	cpu_l_leg = love.graphics.newAnimation( "images/leg_frames.png", 10, 25, 0, 0 )
+	cpu_r_leg = love.graphics.newAnimation( "images/leg_frames.png", 10, 25, 0, 0 )
+	cpuData = {head = 0, l_arm=0, r_arm=0, body=0, l_leg=0, r_leg=0}
 	
 	g_console = Console:new()
 	g_console:init()
@@ -55,27 +72,43 @@ function draw()
 		love.graphics.setColor(255,255,255 )
 		love.graphics.draw(clicktostart, 400, 333)
 		love.graphics.setFont(coolfont)
-		
 	end
 	
 	if screen == "cpu battle" then
-		love.graphics.draw(player_head, 80, 20)
+		love.graphics.rectangle(1,174,14,601,401)
+		love.graphics.draw(battle_view_bg,475,215)
+	
 		love.graphics.draw(player_body, 80, 85)
+		love.graphics.draw(player_head, 80, 25)
 		love.graphics.draw(player_l_arm, 30, 70)
-		love.graphics.draw(player_r_arm, 130, 70)
 		love.graphics.draw(player_r_arm, 130, 70)
 		love.graphics.draw(player_l_leg, 65, 140)
 		love.graphics.draw(player_r_leg, 90, 140)
 		
-		if(playerData.head == 1) then
-			player_head = love.graphics.newImage("images/player_head_green.png")
-		end
-		if(playerData.head == 2) then
-			player_head = love.graphics.newImage("images/player_head_yellow.png")
-		end
-		if(playerData.head == 3) then
-			player_head = love.graphics.newImage("images/player_head_red.png")
-		end
+		love.graphics.draw(cpu_body, 500, 200)
+		love.graphics.draw(cpu_head, 500, 140)
+		love.graphics.draw(cpu_l_arm, 450, 185)
+		love.graphics.draw(cpu_r_arm, 550, 185)
+		love.graphics.draw(cpu_l_leg, 485, 255)
+		love.graphics.draw(cpu_r_leg, 510, 255)
+		
+		player_head:seek(playerData.head)
+		player_body:seek(playerData.body)
+		player_l_arm:seek(playerData.l_arm)
+		player_r_arm:seek(playerData.r_arm)
+		player_l_leg:seek(playerData.l_leg)
+		player_r_leg:seek(playerData.r_leg)
+		
+		cpu_head:seek(cpuData.head)
+		cpu_body:seek(cpuData.body)
+		cpu_l_arm:seek(cpuData.l_arm)
+		cpu_r_arm:seek(cpuData.r_arm)
+		cpu_l_leg:seek(cpuData.l_leg)
+		cpu_r_leg:seek(cpuData.r_leg)
+		
+		love.graphics.draw(shifter, 120, 460)
+		shifter:seek(shifter_position)
+		
 	end
 	
 	g_console:draw()
@@ -122,13 +155,13 @@ function keypressed(key)
 	
 	if screen == "cpu battle" then
 		if key == love.key_1 then
-			playerData.head = 1
+			playerData.head = 0
 		end
 		if key == love.key_2 then
-			playerData.head = 2
+			playerData.head = 1
 		end
 		if key == love.key_3 then
-			playerData.head = 3
+			playerData.head = 2
 		end
 	end
 	
